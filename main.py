10c7ea3a801a4c33644a2dd628ec36838c35acc3
nitaku <matteo.abrate@gmail.com>
2021-09-21 10:01:49 +0200

    final data structure, also saved as file

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import networkx as nx
 import ndjson
+import json
 
+# read ndjson data and create graph
 dag = nx.DiGraph()
 
 with open('data/dictionary.txt', 'r') as f:
@@ -13,6 +15,18 @@ with open('data/dictionary.txt', 'r') as f:
             if parent not in '？⿰⿱⿻⿳⿺⿸⿲⿹⿴⿵⿶⿷':
                 dag.add_edge(parent, row['character'])
 
-print(dag.number_of_nodes())
-generations = [sorted(generation) for generation in nx.topological_generations(dag)]
-print(len(generations))
+# compute topological generations
+generations = nx.topological_generations(dag)
+
+# create the data structure for a tangled tree layout
+data_attribute = nx.get_node_attributes(dag, 'data')
+generations = [[data_attribute[node] for node in sorted(generation)] for generation in generations]
+
+for g in generations:
+    for node in g:
+        node['id'] = node['character']
+        node['parents'] = [parent for parent in dag.predecessors(node['id'])]
+
+# write the data structure to JSON
+with open('data/tangled_tree.json', 'w', encoding='utf8') as f:
+    json.dump(generations, f, ensure_ascii=False)
